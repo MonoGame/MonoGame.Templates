17a9c79c35cd9fcb4093bacf718c62ce4e37ead8
Dominique Louis <savagesoftware@gmail.com>
2024-12-04 17:37:21 +0000

    Migration of 2D StartKit to this repo.

---
+++ CSharp/content/MonoGame.2D.StartKit.CSharp/___SafeGameName___.Core/Settings/BaseSettingsStorage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace ___SafeGameName___.Core.Settings;
+
+public abstract class BaseSettingsStorage : ISettingsStorage
+{
+    protected BaseSettingsStorage()
+    {
+        SettingsFileName = "settings.json"; // Default settings file name
+    }
+
+    protected static Environment.SpecialFolder SpecialFolderPath { get; set; }
+
+    private string settingsFileName;
+    public string SettingsFileName
+    {
+        get => settingsFileName;
+
+        set
+        {
+            if (settingsFileName != value)
+            {
+                settingsFileName = value;
+            }
+        }
+    }
+    protected string SettingsFilePath => Path.Combine(Environment.GetFolderPath(SpecialFolderPath), "___SafeGameName___", SettingsFileName);
+
+    public virtual void SaveSettings<T>(T settings) where T : new()
+    {
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        string jsonString = JsonSerializer.Serialize(settings, options);
+
+        // Ensure that the directory exists
+        string directoryPath = Path.GetDirectoryName(SettingsFilePath);
+        if (!Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+
+        File.WriteAllText(SettingsFilePath, jsonString);
+    }
+
+    public virtual T LoadSettings<T>() where T : new()
+    {
+        if (!File.Exists(SettingsFilePath))
+            return new T();
+
+        string jsonString = File.ReadAllText(SettingsFilePath);
+        return JsonSerializer.Deserialize<T>(jsonString) ?? new T();
+    }
+}
