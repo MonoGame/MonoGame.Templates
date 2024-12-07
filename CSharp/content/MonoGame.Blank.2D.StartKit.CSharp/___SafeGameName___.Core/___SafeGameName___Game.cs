fbd2c184375fb946a6436befc13577ede48ed052
Dominique Louis <savagesoftware@gmail.com>
2024-12-07 13:13:51 +0000

    Initial commit for Blank 2D StartKit.

---
+++ CSharp/content/MonoGame.Blank.2D.StartKit.CSharp/___SafeGameName___.Core/___SafeGameName___Game.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using ___SafeGameName___.Core.Effects;
+using ___SafeGameName___.Core.Localization;
+using ___SafeGameName___.Core.Settings;
+using ___SafeGameName___.ScreenManagers;
+using ___SafeGameName___.Screens;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ___SafeGameName___.Core;
+
+/// <summary>
+/// This is the main entry point for your game
+/// </summary>
+public class ___SafeGameName___Game : Game
+{
+    // Resources for drawing.
+    GraphicsDeviceManager graphicsDeviceManager;
+
+    ScreenManager screenManager;
+
+    SettingsManager<___SafeGameName___Settings> settingsManager;
+    SettingsManager<___SafeGameName___Leaderboard> leaderboardManager;
+    private Texture2D particleTexture;
+    private ParticleManager particleManager;
+
+    public readonly static bool IsMobile = OperatingSystem.IsAndroid() || OperatingSystem.IsIOS();
+    public readonly static bool IsDesktop = OperatingSystem.IsMacOS() || OperatingSystem.IsLinux() || OperatingSystem.IsWindows();
+
+    public ___SafeGameName___Game()
+    {
+        graphicsDeviceManager = new GraphicsDeviceManager(this);
+        // Add it as a service so we can shared 1 instance and grab it as necessary
+        Services.AddService(typeof(GraphicsDeviceManager), graphicsDeviceManager);
+
+        ISettingsStorage storage;
+        // The platform-specific code will be in the platform-specific project
+        if (IsMobile)
+        {
+            storage = new MobileSettingsStorage();
+            graphicsDeviceManager.IsFullScreen = true;
+            IsMouseVisible = false;
+        }
+        else if (IsDesktop)
+        {
+            storage = new DesktopSettingsStorage();
+            graphicsDeviceManager.IsFullScreen = false;
+            IsMouseVisible = true;
+        }
+        else
+        {
+            // For now, we'll throw an exception if we don't know the platform
+            throw new PlatformNotSupportedException();
+        }
+
+        // This will also load the settings file, if it exists
+        settingsManager = new SettingsManager<___SafeGameName___Settings>(storage);
+        Services.AddService(typeof(SettingsManager<___SafeGameName___Settings>), settingsManager);
+
+        leaderboardManager = new SettingsManager<___SafeGameName___Leaderboard>(storage);
+        Services.AddService(typeof(SettingsManager<___SafeGameName___Leaderboard>), leaderboardManager);
+
+        Content.RootDirectory = "Content";
+
+        //graphics.PreferredBackBufferWidth = 800;
+        //graphics.PreferredBackBufferHeight = 480;
+        graphicsDeviceManager.SupportedOrientations = DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight;
+
+        // Create the screen manager component.
+        screenManager = new ScreenManager(this);
+
+        Components.Add(screenManager);
+    }
+
+    /// <summary>
+    /// Override Game.Initialize().  Sets up the ScreenManager.
+    /// </summary>
+    protected override void Initialize()
+    {
+        base.Initialize();
+
+        // Get a list of all languages our app supports
+        List<CultureInfo> cultures = LocalizationManager.GetSupportedCultures();
+        var languages = new List<CultureInfo>();
+        for (int i = 0; i < cultures.Count; i++)
+        {
+            languages.Add(cultures[i]);
+        }
+
+        var selectedLanguage = languages[settingsManager.Settings.Language].Name;
+        LocalizationManager.SetCulture(selectedLanguage);
+
+        // add the background screen to the screen manager
+        screenManager.AddScreen(new BackgroundScreen(), null);
+
+        // add the main menu screen to the screen manager
+        screenManager.AddScreen(new MainMenuScreen(), null);
+    }
+
+    protected override void LoadContent()
+    {
+        // Create a particle manager at the center of the screen
+        particleTexture = Content.Load<Texture2D>("Sprites/blank");
+        particleManager = new ParticleManager(particleTexture, new Vector2(400, 200));
+
+        // Let's reuse the particle manager across the game
+        Services.AddService(typeof(ParticleManager), particleManager);
+    }
+}
