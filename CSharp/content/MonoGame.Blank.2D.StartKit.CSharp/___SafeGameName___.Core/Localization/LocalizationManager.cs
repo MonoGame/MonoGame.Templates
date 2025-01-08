2da863d133a8fa4288740b302d9fad20115ac496
Dominique Louis <savagesoftware@gmail.com>
2025-01-08 23:27:21 +0000

    Document Localization

--- CSharp/content/MonoGame.Blank.2D.StartKit.CSharp/___SafeGameName___.Core/Localization/LocalizationManager.cs
+++ CSharp/content/MonoGame.Blank.2D.StartKit.CSharp/___SafeGameName___.Core/Localization/LocalizationManager.cs
@@ -6,12 +6,21 @@ using System.Threading;
 
 namespace ___SafeGameName___.Core.Localization;
 
+/// <summary>
+/// Manages localization settings for the game, including retrieving supported cultures and setting the current culture for localization.
+/// </summary>
 internal class LocalizationManager
 {
     /// <summary>
-    /// 
+    /// Retrieves a list of supported cultures based on available language resources in the game.
+    /// This method checks the current culture settings and the satellite assemblies for available localized resources.
     /// </summary>
-    /// <returns>A list of Supported CultureInfo, based on what languages we currently install.</returns>
+    /// <returns>A list of <see cref="CultureInfo"/> objects representing the cultures supported by the game.</returns>
+    /// <remarks>
+    /// This method iterates through all specific cultures defined in the satellite assemblies and attempts to load the corresponding resource set.
+    /// If a resource set is found for a particular culture, that culture is added to the list of supported cultures. The invariant culture
+    /// is always included in the returned list as it represents the default (non-localized) resources.
+    /// </remarks>
     public static List<CultureInfo> GetSupportedCultures()
     {
         // Create a list to hold supported cultures
@@ -49,9 +58,21 @@ internal class LocalizationManager
         return supportedCultures;
     }
 
+    /// <summary>
+    /// Sets the current culture of the game based on the specified culture code.
+    /// This method updates both the current culture and UI culture for the current thread.
+    /// </summary>
+    /// <param name="cultureCode">The culture code (e.g., "en-US", "fr-FR") to set for the game.</param>
+    /// <remarks>
+    /// This method modifies the <see cref="Thread.CurrentThread.CurrentCulture"/> and <see cref="Thread.CurrentThread.CurrentUICulture"/> properties,
+    /// which affect how dates, numbers, and other culture-specific values are formatted, as well as how localized resources are loaded.
+    /// </remarks>
     public static void SetCulture(string cultureCode)
     {
+        // Create a CultureInfo object from the culture code
         CultureInfo culture = new CultureInfo(cultureCode);
+
+        // Set the current culture and UI culture for the current thread
         Thread.CurrentThread.CurrentCulture = culture;
         Thread.CurrentThread.CurrentUICulture = culture;
     }
