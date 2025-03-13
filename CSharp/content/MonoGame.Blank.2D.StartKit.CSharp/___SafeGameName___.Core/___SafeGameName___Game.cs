2692e44a1db8c7182cb7624c0ff13fa905cd3f49
Dominique Louis <savagesoftware@gmail.com>
2025-03-13 12:26:44 +0000

    Strip out Blank to be more blank :)

--- CSharp/content/MonoGame.Blank.2D.StartKit.CSharp/___SafeGameName___.Core/___SafeGameName___Game.cs
+++ CSharp/content/MonoGame.Blank.2D.StartKit.CSharp/___SafeGameName___.Core/___SafeGameName___Game.cs
@@ -1,13 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Globalization;
-using ___SafeGameName___.Core.Effects;
-using ___SafeGameName___.Core.Localization;
-using ___SafeGameName___.Core.Settings;
-using ___SafeGameName___.ScreenManagers;
-using ___SafeGameName___.Screens;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace ___SafeGameName___.Core;
 
@@ -19,18 +12,6 @@ public class ___SafeGameName___Game : Game
 {
     // Resources for drawing.
     private GraphicsDeviceManager graphicsDeviceManager;
-
-    // Manages the game's screen transitions and screens.
-    private ScreenManager screenManager;
-
-    // Manages game settings, such as preferences and configurations.
-    private SettingsManager<___SafeGameName___Settings> settingsManager;
-
-    // Texture for rendering particles.
-    private Texture2D particleTexture;
-
-    // Manages particle effects in the game.
-    private ParticleManager particleManager;
 
     /// <summary>
     /// Indicates if the game is running on a mobile platform.
@@ -54,37 +35,10 @@ public class ___SafeGameName___Game : Game
         // Share GraphicsDeviceManager as a service.
         Services.AddService(typeof(GraphicsDeviceManager), graphicsDeviceManager);
 
-        // Determine the appropriate settings storage based on the platform.
-        ISettingsStorage storage;
-        if (IsMobile)
-        {
-            storage = new MobileSettingsStorage();
-            graphicsDeviceManager.IsFullScreen = true;
-            IsMouseVisible = false;
-        }
-        else if (IsDesktop)
-        {
-            storage = new DesktopSettingsStorage();
-            graphicsDeviceManager.IsFullScreen = false;
-            IsMouseVisible = true;
-        }
-        else
-        {
-            throw new PlatformNotSupportedException();
-        }
-
-        // Initialize settings and leaderboard managers.
-        settingsManager = new SettingsManager<___SafeGameName___Settings>(storage);
-        Services.AddService(typeof(SettingsManager<___SafeGameName___Settings>), settingsManager);
-
         Content.RootDirectory = "Content";
 
         // Configure screen orientations.
         graphicsDeviceManager.SupportedOrientations = DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight;
-
-        // Initialize the screen manager.
-        screenManager = new ScreenManager(this);
-        Components.Add(screenManager);
     }
 
     /// <summary>
@@ -94,20 +48,6 @@ public class ___SafeGameName___Game : Game
     protected override void Initialize()
     {
         base.Initialize();
-
-        // Load supported languages and set the default language.
-        List<CultureInfo> cultures = LocalizationManager.GetSupportedCultures();
-        var languages = new List<CultureInfo>();
-        for (int i = 0; i < cultures.Count; i++)
-        {
-            languages.Add(cultures[i]);
-        }
-        var selectedLanguage = languages[settingsManager.Settings.Language].Name;
-        LocalizationManager.SetCulture(selectedLanguage);
-
-        // Add background and gameplay screens.
-        screenManager.AddScreen(new BackgroundScreen(), null);
-        screenManager.AddScreen(new GameplayScreen(), null);
     }
 
     /// <summary>
@@ -116,12 +56,39 @@ public class ___SafeGameName___Game : Game
     protected override void LoadContent()
     {
         base.LoadContent();
+    }
 
-        // Load a texture for particles and initialize the particle manager.
-        particleTexture = Content.Load<Texture2D>("Sprites/blank");
-        particleManager = new ParticleManager(particleTexture, new Vector2(400, 200));
+    /// <summary>
+    /// Updates the game's logic, called once per frame.
+    /// </summary>
+    /// <param name="gameTime">
+    /// Provides a snapshot of timing values used for game updates.
+    /// </param>
+    protected override void Update(GameTime gameTime)
+    {
+        // Exit the game if the Back button (GamePad) or Escape key (Keyboard) is pressed.
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed
+            || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
 
-        // Share the particle manager as a service.
-        Services.AddService(typeof(ParticleManager), particleManager);
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    /// <summary>
+    /// Draws the game's graphics, called once per frame.
+    /// </summary>
+    /// <param name="gameTime">
+    /// Provides a snapshot of timing values used for rendering.
+    /// </param>
+    protected override void Draw(GameTime gameTime)
+    {
+        // Clears the screen with the MonoGame orange color before drawing.
+        GraphicsDevice.Clear(Color.MonoGameOrange);
+
+        // TODO: Add your drawing code here
+
+        base.Draw(gameTime);
     }
 }
