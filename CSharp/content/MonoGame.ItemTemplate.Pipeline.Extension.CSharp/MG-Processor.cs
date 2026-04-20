6dd2e30b1de07bf5991761b0e4c18788887caa79
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2026-04-20 13:57:12 +0100

    Feature/simondarksidej/itemtemplates (#37)
    * Added SpriteFont item template
    
    * Added Additional Extension item template
    
    * Remove trailing line

---
+++ CSharp/content/MonoGame.ItemTemplate.Pipeline.Extension.CSharp/MG-Processor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline;
+
+using TInput = System.String;
+using TOutput = System.String;
+
+[ContentProcessor(DisplayName = "MGProcessor")]
+public class MGProcessor : ContentProcessor<TInput, TOutput>
+{
+    public override TOutput Process(TInput input, ContentProcessorContext context)
+    {
+        return default(TOutput);
+    }
+}
